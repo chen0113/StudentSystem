1cb31ee168016b4840f1d23eb0be659dedc94fb4
chenyong <993844085@qq.com>
2019-11-30 17:10:52 +0800

    111

---
+++ src/test/java/com/ccxg/studentsystem/StudentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccxg.studentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
